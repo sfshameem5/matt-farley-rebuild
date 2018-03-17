33b0375e1a9de0bbb1d96009e9fc3db87d6bd29f
Shameem Fairooz <sfshameem5@gmail.com>
2018-03-17 12:57:30 +0530

    Finished Project

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+var options = {
+  strings: ["landing pages.", "corporate websites.", "web applications.", "user interfaces."],
+  startDelay: 300,
+  typeSpeed: 120,
+  loop: true,
+  loopCount: Infinity,
+  fadeOut: true,
+  fadeOutClass: 'typed-fade-out',
+  fadeOutDelay: 500,
+  autoInsertCss: true
+}
+
+var typed = new Typed(".typer", options);
